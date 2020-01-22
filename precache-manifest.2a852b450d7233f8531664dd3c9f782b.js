165feb072de8938f78f74221528728ff18d1ccec
Guilherme Moreno <guilherme@pagolivre.com.br>
2020-01-22 14:43:42 -0300

    Updates

---
+++ precache-manifest.2a852b450d7233f8531664dd3c9f782b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2282358ecce568c699ff3a77d2917ba",
+    "url": "/GHPages/index.html"
+  },
+  {
+    "revision": "09f552f5636e7ca2060c",
+    "url": "/GHPages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "651907a7e02234f7cfcc",
+    "url": "/GHPages/static/js/2.33af256b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/GHPages/static/js/2.33af256b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09f552f5636e7ca2060c",
+    "url": "/GHPages/static/js/main.90691df2.chunk.js"
+  },
+  {
+    "revision": "e50a82ebdb1eddc6a670",
+    "url": "/GHPages/static/js/runtime-main.e960bafc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GHPages/static/media/logo.5d5d9eef.svg"
+  }
+]);
